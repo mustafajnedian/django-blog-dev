772aa261ce6f7e5ab210e07569e6968311676b54
Mustafa Jahangir <52215179+mustafajnedian@users.noreply.github.com>
2025-02-06 21:06:28 +0500

    Initial commit

---
+++ install-env.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt install python3-django
+sudo apt install python3-djangorestframework
+sudo apt install python3-psycopg2
+sudo apt update
+sudo apt install python3-django
+django-admin --version
+sudo apt update
+sudo apt install python3 python3-pip
+#django-admin startproject myblog
+#cd myblog
+#django-admin startapp blog
+
